fc98cde6b68dc5ba28ed5382cc932d854c032382
TheTrunk <tadeas.kmenta@zel.cash>
2023-08-15 08:24:50 +0200

    adjust generation

--- helpers/createSSLcert.sh
+++ helpers/createSSLcert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# openssl genrsa -out ../certs/v1privkey.pem 2048
-# openssl req -new -key ../certs/v1privkey.pem -out ../certs/v1csr.pem -subj '/CN=self.api.runonflux.io/O=RunOnFlux'
-# openssl x509 -req -in ../certs/v1csr.pem -CA ../certs/v1rootcert.cert.pem -CAkey ../certs/v1rootcert.privkey.pem -CAcreateserial -out ../certs/v1cert.pem -days 3650
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../certs/v1.key -out ../certs/v1.crt -subj '/CN=self.api.runonflux.io/O=RunOnFlux' > /dev/null 2>&1
+IP=$(curl -s https://ipify.app.runonflux.io)
+openssl genrsa -out ../certs/v1privkey.pem 2048
+openssl req -new -key ../certs/v1privkey.pem -out ../certs/v1.key -subj '/CN=self.api.runonflux.io/O=RunOnFlux'
+openssl x509 -req -in ../certs/v1.key -CA ../certs/v1rootcert.cert.pem -CAkey ../certs/v1rootcert.privkey.pem -CAcreateserial -out ../certs/v1.crt -days 3650
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../certs/v1.key -out ../certs/v1.crt -subj '/CN=self.api.runonflux.io/O=RunOnFlux' -addext "subjectAltName = DNS:self.api.runonflux.io, IP:$IP"
 sleep 2
